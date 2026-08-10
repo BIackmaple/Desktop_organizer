dbe9af7c08d98590a9129081c0bba9e3f5e41476
BIackmaple <k18204543072@163.com>
2026-08-10 10:24:44 +0800

    增添了坐标功能

--- Desktop_organizer/main.cpp
+++ Desktop_organizer/main.cpp
@@ -1,12 +1,33 @@
-#include <windows.h>
+﻿#include <windows.h>
+#include <cwchar>
 
 //窗口消息处理函数
 //目标窗口，消息类型，附加参数1，附加参数2
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
+		case WM_DESTROY:{
+			PostQuitMessage(0);
+			return 0;
+		}
+		case WM_LBUTTONDOWN:{
+			int x = LOWORD(lParam);
+			int y = HIWORD(lParam);
+
+			wchar_t text[100];
+			swprintf_s(
+				text,
+				L"鼠标点击位置：(%d, %d)",
+				x,
+				y
+			);
+			MessageBox(
+				hwnd,
+				text,
+				L"Mouse",
+				MB_OK
+			);
+			return 0;
+		}
 	}
 	return DefWindowProc(hwnd, msg, wParam, lParam);
 }
@@ -42,3 +63,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 	return 0;
 }
+
